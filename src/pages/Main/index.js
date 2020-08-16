3d0af8b069ec7603722bcfc2865ba7efc76c2425
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-08-16 10:37:02 -0300

    Add global styles & Main page component style

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
-  return <Title>Main Page</Title>
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input
+          type="text"
+          placeholder="Adicionar repositório"
+        />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 };
 
 export default Main;
